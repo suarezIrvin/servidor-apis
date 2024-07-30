62fb8180eb98fe30f23e84cb5d2f9e9933c94756
Christian Navarro <christianyahirnavarrochan@gmail.com>
2024-07-29 21:25:12 -0600

    cambios al detalle evento nuevamente

--- src/models/detallesEventoModel.js
+++ src/models/detallesEventoModel.js
@@ -2,10 +2,20 @@ const pool = require('../config/connection');
 
 const getDetallesEventoByEventoId = async (evento_id) => {
     const query = `
-      SELECT DE.*, E.nombre AS nombre_evento 
-      FROM Detalles_Evento DE
-      JOIN Eventos E ON DE.evento_id = E.evento_id
-      WHERE DE.evento_id = ?
+      SELECT 
+    a.detalle_evento_id, a.evento_id, a.descripcion, a.requerimientos, a.Precio, 
+    b.nombre as nombre_evento, b.fecha_inicio, b.fecha_termino, b.hora, b.ubicacion, b.max_per,
+    c.nombre as tipo_evento,
+    d.nombre as categoria,
+    e.nombre as organizador_nombre,
+    f.imagen_url 
+    FROM Detalles_Evento a
+    INNER JOIN Eventos b ON a.evento_id = b.evento_id
+    INNER JOIN Tipos_Evento c ON b.tipo_evento_id = c.tipo_evento_id
+    INNER JOIN Categorias d ON b.categoria_id = d.categoria_id
+    INNER JOIN Usuarios e ON b.organizador_id = e.usuario_id
+    INNER JOIN Imagenes f ON b.evento_id = f.evento_id
+    WHERE b.evento_id = ?
     `;
     try {
       const [results] = await pool.query(query, [evento_id]);
