82ee0bc688ab9e1cccedc1f7163750553982c2fd
Christian Navarro <christianyahirnavarrochan@gmail.com>
2024-08-12 15:20:57 -0600

    pagos mejorado

--- src/controllers/pagoController.js
+++ src/controllers/pagoController.js
@@ -80,8 +80,88 @@ const historialPagos = async (req, res) => {
   }
 }
 
+//codigo mejorado
+
+const PagoMejorado = async (req, res) => {
+  const { body } = req;
+
+  const { amount, currency, descripcion, usuario_id, evento_id } = body;
+  if (!amount || !currency || !descripcion || !usuario_id || !evento_id) {
+    return res.status(400).json({ message: "Faltan parámetros necesarios" });
+  }
+
+  try{
+      const paymentIntent = await stripe.paymentIntents.create({
+          amount: amount,
+          currency: currency,
+          description: descripcion,
+          automatic_payment_methods: {
+            enabled: true,
+            allow_redirects: "never",
+          },
+        });
+    
+  
+        // Realizar la inserción en la base de datos
+        const [rows, fields] = await pool.execute(
+          'INSERT INTO Pagos (monto, fecha, tipo_pago_id, usuario_id, evento_id) VALUES (?, CURDATE(), 1, ?, ?)',
+          [amount, usuario_id, evento_id]
+        );
+    
+        const pagoId = rows.insertId;
+    
+        // Insertar en la tabla Pago_Tarjeta
+        await pool.execute(
+          'INSERT INTO Pago_Tarjeta (numero_tarjeta, fecha_expiracion, cvv, pago_id) VALUES (424242, CURDATE(), 123, ?)',
+          [pagoId]
+        );
+    
+        // Verificar el estado del paymentIntent y enviar la respuesta correspondiente
+        if (paymentIntent?.status !== 'completed') {
+          return res.status(200).json({
+            message: "Confirma tu pago",
+            paymentIntentId: paymentIntent?.id, 
+            client_secret: paymentIntent?.client_secret,
+            //client_secret o id
+          });
+        } else {
+          return res.status(200).json({ message: "Pago completado" });
+        } 
+
+  } catch (error) {
+    console.error('Error en el método POST:', error);
+    const errorMessage = error.raw?.message || error.message || "Unknown error occurred";
+    return res.status(500).json({ message: errorMessage });
+  }
+}
+
+const historialPagosPorUsuarioID = async (req, res) => {
+  const { usuario_id } = req.params;
+
+  if (!usuario_id) {
+    return res.status(400).send('Falta el parámetro usuario_id');
+  }
+  try {
+    const [result] = await pool.query(
+    `
+        SELECT Pagos.*, Pago_Tarjeta.numero_tarjeta, Pago_Tarjeta.fecha_expiracion, Pago_Tarjeta.cvv
+        FROM Pagos
+        LEFT JOIN Pago_Tarjeta ON Pagos.pago_id = Pago_Tarjeta.pago_id
+        WHERE Pagos.user_id = ?
+    `,
+    [usuario_id]
+    );
+    res.status(200).json(result);
+  } catch (error) {
+    console.error('Error al obtener el historial:', error);
+    res.status(500).send('Error al obtener el historial');
+  }
+}
+
 module.exports = {
     Pago,
     confirmarPago,
-    historialPagos
+    historialPagos,
+    PagoMejorado,
+    historialPagosPorUsuarioID,
   };
--- src/routes/pagoRouter.js
+++ src/routes/pagoRouter.js
@@ -148,6 +148,123 @@ router.post('/confirmarpago', pagoController.confirmarPago);
  *                       cvv:
  *                         type: string
  */
-router.get('/historialpagos', pagoController.historialPagos)
+router.get('/historialpagos', pagoController.historialPagos);
+
+
+
+
+/**
+ * @openapi
+ * /api/pagos/pagar:
+ *   post:
+ *     summary: Nuevo api de pagos mejorado con mas datos para pasar.
+ *     tags:
+ *       - Pagos
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               amount:
+ *                 type: integer
+ *                 description: Monto del pago en centavos.
+ *               currency:
+ *                 type: string
+ *                 description: Moneda del pago (ej. USD).
+ *                 enum:
+ *                   - "usd"
+ *               descripcion:
+ *                 type: string
+ *                 description: Descripcion del producto, nombre del evento o producto
+ *               usuario_id:
+ *                 type: int
+ *                 description: id del usuario que esta pagondo
+ *               evento_id:
+ *                 type: int
+ *                 description: id del evento que se compra
+ *     responses:
+ *       '200':
+ *         description: Respuesta exitosa. Devuelve el client_secret para confirmar el pago.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Mensaje de confirmación.
+ *                 client_secret:
+ *                   type: string
+ *                   description: Clave secreta del cliente para confirmar el pago.
+ *       '500':
+ *         description: Error interno del servidor.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Mensaje de error detallado.
+ */
+router.post('/pagar', pagoController.PagoMejorado);
+
+
+/**
+ * @openapi
+ * /api/pagos/historial/:usuario_id:
+ *   get:
+ *     summary: obtiene el historial de un usuario por medio de su id
+ *     description: Obtiene el historial por medio del id del usuario
+ *     tags:
+ *       - Pagos
+ *     parameters:
+ *       - name: usuario_id
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         example: 1
+ *     responses:
+ *       200:
+ *         description: Detalles del historial de pago de un usuario encontrado.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                   pago_id:
+ *                     type: integer
+ *                   monto:
+ *                     type: string
+ *                   fecha:
+ *                     type: string
+ *                     format: date
+ *                   tipo_pago_id:
+ *                     type: integer
+ *                   usuario_id:
+ *                     type: integer
+ *                   evento_id:
+ *                     type: integer
+ *                   tarjeta:
+ *                     type: object
+ *                     properties:
+ *                       tarjeta_id:
+ *                         type: integer
+ *                       numero_tarjeta:
+ *                         type: string
+ *                       fecha_expiracion:
+ *                         type: string
+ *                         format: date
+ *                       cvv:
+ *                         type: string
+ *       404:
+ *         description: historial no encontrado.
+ *       500:
+ *         description: Error al obtener el historial.
+ */
+router.get('/historial/:usuario_id', pagoController.historialPagosPorUsuarioID);
 
 module.exports = router;
